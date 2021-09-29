fc4b17bcbfe8f5e3fdd2244ac50c145e5030bd57
Krutik Thaker <kthaker17@gmail.com>
2021-09-29 10:02:23 +0530

    styling

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,4 +2,13 @@ class Gallery < ApplicationRecord
 
     belongs_to :user
     mount_uploader :image, ImageUploader
+    validate :image_size_validation
+    validates :image, presence: true
+    validates :title, presence: true
+
+    private
+        def image_size_validation
+            errors[:image] << "should be less than 500KB" if image.size > 0.5.megabytes
+        end
+        
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,5 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+ 
+
 
 end
