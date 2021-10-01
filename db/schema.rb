554fb2a1a680c595210bca587dfb381b582dd583
Krutik Thaker <kthaker17@gmail.com>
2021-10-01 15:44:09 +0530

    change db to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_09_27_084557) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "galleries", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
